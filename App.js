a4e26eec3c7f08ea8e1a553a84b5676835257ab2
Louis <louis@Louiss-Mac-mini.lan>
2020-05-25 09:45:51 -0400

    Added ProductsOverviewScreen and ProductsNavigator

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
+
+import productReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
+
+const rootReducer = combineReducers({
+  products: productReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
 
