f47c0545f8322ad579407e97958da908a7d0fe31
Louis <louis@Louiss-Mac-mini.lan>
2020-05-30 17:34:08 -0400

    Auto login users if token has not expired

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -15,6 +15,7 @@ import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import EditProductScreen from '../screens/user/EditProductScreen';
 import AuthScreen from '../screens/user/AuthScreen';
+import StartupScreen from '../screens/StartupScreen';
 
 const defaultNavOptions = {
   headerStyle: {
@@ -109,6 +110,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const MainNavigator = createSwitchNavigator({
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator,
 });
---
+++ screens/StartupScreen.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  AsyncStorage,
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+import * as authActions from '../store/actions/auth';
+
+import Colors from '../constants/Colors';
+
+const StartupScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const tryLogin = async () => {
+      const userData = await AsyncStorage.getItem('userData');
+      if (!userData) {
+        navigation.navigate('Auth');
+        return;
+      }
+
+      const transformedData = JSON.parse(userData);
+      const { token, userId, expiryDate } = transformedData;
+
+      const expirationDate = new Date(expiryDate);
+
+      // check if expirationDate is in the past (which means token has expired)
+      if (expirationDate <= new Date() || !token || !userId) {
+        navigation.navigate('Auth');
+        return;
+      }
+
+      navigation.navigate('Shop');
+      dispatch(authActions.authenticate(userId, token));
+    };
+
+    tryLogin();
+  }, [dispatch]);
+
+  return (
+    <View style={styles.screen}>
+      <ActivityIndicator size="large" color={Colors.primary} />
+    </View>
+  );
+};
+
+export default StartupScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
